9de8e42c70caa9752ae094ffa6aa11ed5887e9c9
Mario Jesus <mariojesusariashernandez@gmail.com>
2024-11-30 16:01:09 -0600

    Update

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,22 @@
-console.log("Hello world!");
+window.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  const desktopShareButton = document.getElementById("desktopShareButton");
+  const desktopShareWindow = document.getElementById("desktopShareWindow");
+  const movilShareButton = document.getElementById("movilShareButton");
+  const movilShareWindow = document.getElementById("movilShareWindow");
+  if (!desktopShareButton || !movilShareButton || !desktopShareWindow || !movilShareWindow) return;
+
+  document.addEventListener("click", (e) => {
+    // @ts-ignore
+    if ((desktopShareButton.contains(<HTMLElement>e.target) || desktopShareWindow.contains(<HTMLElement>e.target)) && !(e.target.nodeName == "IMG")) {
+      desktopShareButton.dataset.active = "true";
+    // @ts-ignore
+    } else if ((movilShareButton.contains(<HTMLElement>e.target) || movilShareWindow.contains(<HTMLElement>e.target)) && !(e.target.nodeName == "IMG")) {
+      movilShareButton.dataset.active = "true";
+    } else {
+      desktopShareButton.dataset.active = "false";
+      movilShareButton.dataset.active = "false";
+    }
+  });
+}
